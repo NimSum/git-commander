44e550ca6bb8a1b0758e53bef924c85cae12d2b6
Nimrod Garcia <nimm.garcia@gmail.com>
2019-04-12 11:00:04 -0600

    Initial Commit

---
+++ src/header/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>GIT COMMANDER</h1>
+        <p>HELLOOOOOOO</p>
+      </header>
+    )
+  }
+}
