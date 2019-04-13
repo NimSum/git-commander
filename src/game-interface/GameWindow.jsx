5e90d26bcd35c38d826e1a7f51e1dfac77fa6b72
Nimrod Garcia <nimm.garcia@gmail.com>
2019-04-13 14:21:21 -0600

    Add octo ship

--- src/game-interface/GameWindow.jsx
+++ src/game-interface/GameWindow.jsx
@@ -5,13 +5,17 @@ export class GameWindow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      shipPosition: 4
     }
   }
   render() {
     return (
       <section className="game-window">
         <img className="obstacle" src="http://clipart-library.com/image_gallery/n756983.gif" alt="obstacle" />
+        <img 
+          className="octo-ship" 
+          src={ require("../images/jetpacktocat.png") }
+          alt="obstacle" />
         < ProgressIndicator 
           currRound={ this.props.currRound } />
       </section>
