9737cd8c1d89b6f229ce19c77a31bfbc5f15438c
Nimrod Garcia <nimm.garcia@gmail.com>
2019-04-13 16:00:54 -0600

    Git ship location, obstacle randomizer

--- src/game-interface/GameWindow.jsx
+++ src/game-interface/GameWindow.jsx
@@ -5,15 +5,31 @@ export class GameWindow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      shipPosition: 4
+      shipPosition: 1,
+      clearPath: Math.round(Math.random() * (5 - 1) + 1),
+      obstaclePositions: [1, 2, 3, 4, 5]
     }
   }
+
+  generateObstacles() {
+    const obstacleContainer = []
+    for (let i = 0; i <= this.props.currRound; i++) {
+      obstacleContainer.push(
+        ( <img 
+          className={`obstacle obstacle-pos-${i+1}${Math.round(Math.random())}`}
+          src="http://clipart-library.com/image_gallery/n756983.gif" 
+          alt="obstacle" />)
+      )
+    }
+    return obstacleContainer;
+  }
+
   render() {
     return (
       <section className="game-window">
-        <img className="obstacle" src="http://clipart-library.com/image_gallery/n756983.gif" alt="obstacle" />
+        {this.generateObstacles()}
         <img 
-          className="octo-ship" 
+          className={`octo-ship ship-position-${this.state.shipPosition}` }
           src={ require("../images/jetpacktocat.png") }
           alt="obstacle" />
         < ProgressIndicator 
