8b0e46de48ca60306a6f6cfa6d28b0ac97dbd993
Nimrod Garcia <nimm.garcia@gmail.com>
2019-04-13 20:06:11 -0600

    Randomize obstacle locations, set git ship to collision course

--- src/game-interface/ProgressIndicator.jsx
+++ src/game-interface/ProgressIndicator.jsx
@@ -4,18 +4,18 @@ export function ProgressIndicator(props) {
   return (
     <div className="progress-indicator">
       <span
-      className={props.currRound >= 6 && "current-round"}>Remote</span>
+      className={props.currRound >= 6 ? "current-round" : undefined}>Remote</span>
       <span
-      className={props.currRound >= 5 && "current-round"}></span>
+      className={props.currRound >= 5 ? "current-round" : undefined}></span>
       <span
-      className={props.currRound >= 4 && "current-round"}></span>
+      className={props.currRound >= 4 ? "current-round" : undefined}></span>
       <div className="vertical-line"></div>
       <span
-      className={props.currRound >= 3 && "current-round"}></span>
+      className={props.currRound >= 3 ? "current-round" : undefined}></span>
       <span
-      className={props.currRound >= 2 && "current-round"}></span>
+      className={props.currRound >= 2 ? "current-round" : undefined}></span>
       <span 
-      className={props.currRound >= 1 && "current-round"}>Local</span>
+      className={props.currRound >= 1 ? "current-round" : undefined}>Local</span>
     </div>
   )
 }
