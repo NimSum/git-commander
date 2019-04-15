6a1892cc6c5b816866215b6292f95f7d814bd855
Nimrod Garcia <nimm.garcia@gmail.com>
2019-04-14 18:49:03 -0600

    App component testing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,21 +3,46 @@ import App from '../App';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow , configure } from 'enzyme';
 
-// configure({adapter: new Adapter()});
+configure({adapter: new Adapter()});
 
-// describe('Challenge card' , () => {
+describe('Challenge card' , () => {
 
 
-//   let wrapper;
+  let wrapper;
+  const defaultState = {
+    challenges: [],
+    currentRound: 1,
+    startGame: false,
+    playerName: ''
+  }
 
-//   beforeEach(() => {
-//     wrapper = shallow(
+  beforeEach(() => {
+    wrapper = shallow(
+      < App />
+    );
+  });
 
-//     );
-//   });
+  it('Should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
 
-//   it('Should render a new challenge', () => {
+  it('Should have default state properties', () => {
+    expect(wrapper.state()).toEqual(defaultState);
+  })
 
-//   })
+  it('Should be able to start game', () => {
+    wrapper.instance().startGame('NIMSUM');
+    expect(wrapper.state().playerName).toEqual('NIMSUM');
+    expect(wrapper.state().startGame).toEqual(true);
+  })
 
-// })
+  it('Should be able to change round', () => {
+    wrapper.instance().nextRound();
+    expect(wrapper.state().currentRound).toEqual(2);
+    wrapper.instance().nextRound();
+    expect(wrapper.state().currentRound).toEqual(3);
+    wrapper.instance().nextRound();
+    expect(wrapper.state().currentRound).toEqual(4);
+  })
+
+})
